4ba5bdb6f9a14456c082e11884e494375f2ee75e
lindamirlldeoliveira-dot <lindamirlldeoliveira@gmail.com>
2025-11-17 05:12:48 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// MENU HAMBÚRGUER
+const btnMobile = document.getElementById("btn-mobile");
+const menu = document.getElementById("menu");
+
+btnMobile.addEventListener("click", () => {
+    menu.classList.toggle("open");
+});
+
+// DROPDOWN MOBILE
+document.addEventListener("click", (e) => {
+    const dropdown = document.querySelector(".dropdown");
+    if (!dropdown.contains(e.target)) {
+        dropdown.classList.remove("open");
+    }
+});
+
+document.querySelector(".dropdown-btn").addEventListener("click", () => {
+    document.querySelector(".dropdown").classList.toggle("open");
+});
