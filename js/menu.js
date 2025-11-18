14e21e122fcdc5fdd8ae58b3bda4b747ef3ba286
lindamirlldeoliveira-dot <lindamirlldeoliveira@gmail.com>
2025-11-17 23:45:01 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    /* ===========================
-       MENU HAMBÚRGUER (MOBILE)
-    =========================== */
+   
     const btnMobile = document.getElementById("btn-mobile");
     const menu = document.getElementById("menu");
 
@@ -13,18 +11,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 
-    /* ===========================
-       DROPDOWN DO MENU "PROJETOS"
-    =========================== */
     const dropdown = document.querySelector(".dropdown");
 
     if (dropdown) {
         const btnDropdown = dropdown.querySelector(".dropdown-btn, a"); 
-        // aceita botão ou <a> dependendo do HTML
+
 
         if (btnDropdown) {
             btnDropdown.addEventListener("click", (e) => {
-                // No desktop o hover já abre, então este JS só atua no MOBILE
+                
                 if (window.innerWidth <= 768) {
                     e.preventDefault();  
                     dropdown.classList.toggle("open");
@@ -33,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         }
 
-        // FECHA dropdown se clicar fora
+   
         document.addEventListener("click", (e) => {
             if (!dropdown.contains(e.target)) {
                 dropdown.classList.remove("open");
